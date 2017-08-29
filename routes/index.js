54585c1f75dc043b45df6f2082aee4590e055364
Alan Wilder <wilderalan@gmail.com>
2017-08-29 14:45:14 -0400

    finish the app but its not working

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+
+var jwt = require('express-jwt');
+var auth = jwt({secret: 'SECRET', UserProperty: 'payload'});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,7 +13,7 @@ router.get('/', function(req, res, next) {
 var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
-
+var User = mongoose.model('User');
 router.get('/posts', function(req, res, next) {
   Post.find(function(err, posts) {
     if(err){ return next(err); }
@@ -18,8 +22,9 @@ router.get('/posts', function(req, res, next) {
   })
 })
 
-router.post('/posts', function(req, res, next) {
+router.post('/posts', auth, function(req, res, next) {
   var post = new Post(req.body);
+  post.author = req.payload.username;
 
   post.save(function(err, post){
     if(err) { return next(err); }
@@ -33,7 +38,7 @@ router.param('post', function(req, res, next, id) {
 
   query.exec(function(err, post) {
     if (err) { return next(err); }
-    if(!post) { return next(new Error('can\' find post')); }
+    if(!post) { return next(new Error('can\'t find post')); }
 
     req.post = post;
     return next();
@@ -43,9 +48,9 @@ router.param('post', function(req, res, next, id) {
 router.param('comment', function(req, res, next, id) {
   var query = Comment.findById(id);
 
-  query.exec(function(err, post) {
+  query.exec(function(err, comment) {
     if (err) { return next(err); }
-    if(!comment) { return next(new Error('can\' find comment')); }
+    if(!comment) { return next(new Error('can\'t find comment')); }
 
     req.comment = comment;
     return next();
@@ -61,7 +66,7 @@ router.get('/posts/:post', function(req, res) {
   });
 });
 
-router.put('/posts/:post/upvote', function(req, res, next) {
+router.put('/posts/:post/upvote', auth, function(req, res, next) {
   req.post.upvote(function(err, post){
     if (err) return next(err);
 
@@ -69,9 +74,10 @@ router.put('/posts/:post/upvote', function(req, res, next) {
   });
 });
 
-router.post('/posts/:post/comments', function(req, res, next) {
+router.post('/posts/:post/comments', auth, function(req, res, next) {
   var comment = new Comment(req.body);
   comment.post = req.post;
+  comment.author = req.payload.username;
 
   comment.save(function(err, comment) {
     if(err) return next(err);
@@ -84,12 +90,45 @@ router.post('/posts/:post/comments', function(req, res, next) {
   });
 });
 
-router.put('/posts/:post/:comment/upvote', function(req, res, next) {
-  req.comment.upvote(function(err, post){
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
+  req.comment.upvote(function(err, comment){
     if (err) return next(err);
 
     res.json(comment);
   });
+});
+
+router.post('/register', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  var user = new User();
+
+  user.username = req.body.username;
+
+  user.setPassword(req.body.password)
+
+  user.save(function (err){
+    if(err){ return next(err); }
+
+    return res.json({token: user.generateJWT()})
+  });
+});
+
+router.post('/login', function(req, res, next) {
+  if(!req.body.username || !req.body.password) {
+    return res.status(400).json({message: "Please fill out all fields"});
+  }
+
+  passport.authenticate('local', function(err, user, info) {
+    if(err) return next(err);
+    if(user) {
+      return res.json({token: user.generateJWT()});
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
 });
 
 
